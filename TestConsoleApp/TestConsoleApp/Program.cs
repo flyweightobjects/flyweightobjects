bd4d21b527c7c77b602c9545a2636c18fb4fa97d
ebrand <eric.d.brand@gmail.com>
2014-11-13 14:42:05 -0600

    Wow, this is happening.

--- TestConsoleApp/TestConsoleApp/Program.cs
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestConsoleApp
 {
@@ -11,6 +7,7 @@ namespace TestConsoleApp
         static void Main(string[] args)
         {
             // this is a comment
+            // yet another comment, this is ridiculous
         }
     }
 }
